47a390e0c5e44c9c8f32bc697e68643f69c36d1a
Patryk <desantowski@gmail.com>
2020-02-28 08:47:24 +0100

    Dodano komentarze, przebudowano kolejne części kodu

--- manager.py
+++ manager.py
@@ -9,10 +9,10 @@ import subprocess
 import utilities
 #import kivy.app
 
+with open('lic-4/courses.json') as g:
+    courses = json.loads(g.read())
 with open('c/_name', "r+") as f:
-    with open('lic-4/courses.json') as g:
-        courses = json.loads(g.read())
-        print("### Aktywny kurs: " + courses[f.read()])
+    print("### Aktywny kurs: " + courses[f.read()])
 
 print("1. Pracuj nad kursem")
 print("2. Zmień aktywny kurs")
@@ -43,8 +43,6 @@ if choice == '1':  # Pracuj nad kursem
         lectures[len(lectures) + 1] = name
         utilities.AppendNewLecture(lectures, len(lectures))
         utilities.RewriteFileUncommented("c/master.tex")
-        with open('c/l' + str(len(lectures) ) + '.tex', "w+") as f:
-            f.write("")
         procManager.OpenLectureFile(len(lectures))
 
     else: #coś innego niż nowa notatka
@@ -52,17 +50,12 @@ if choice == '1':  # Pracuj nad kursem
         procManager.OpenLectureFile(choice2)
 
 if choice == '2':  # Zmień aktywny kurs
-    with open('lic-4/courses.json') as f:
-        courses = json.loads(f.read())
-        print(courses)
-        iter = 0
-        courses_i = dict()
-        for i in courses:
-            iter = iter + 1
-            print(" ", iter, ") " + courses[i], sep='')
-            courses_i[str(iter)] = i
-        print("[", iter + 1, "] Nowy kurs", sep='')
-        print(courses_i)
+     # courses_i mapuje ID -> nazwa robocza (folderu) kursu, courses mapuje folder -> nazwa
+    [courses, courses_i] = utilities.GetCoursesMapping()
+    for iter, name in courses_i.items():
+        print(" ", iter, ") " + courses[name], sep='')
+    print("[", str(len(courses_i) + 1), "] Nowy kurs", sep='')
+    # print(courses_i)
     choice = input("Wybieram kurs: ")
     if choice not in courses_i:
         name = input("Nazwa nowego kursu: ")
@@ -71,21 +64,7 @@ if choice == '2':  # Zmień aktywny kurs
             courses[folder] = name
             with open('lic-4/courses.json', "w+") as f:
                 f.write(json.dumps(courses))
-            print("cmd /c mkdir lic-4/" + folder)
-            os.system("cmd /c mkdir lic-4\\" + folder)
-            os.system("cmd /c rmdir c")
-            os.system("cmd /c mklink /j c \"lic-4\\" + folder + "\"")
-            with open('c/master.tex', "w+") as f:
-                f.write(
-                    "\\documentclass[12pt]{article}\n\\input{../preamble.tex}\n\\input{coursepreamble.tex}\n\\begin{document}\n\n\\end{document}")
-            with open('c/coursepreamble.tex', "w+") as f:
-                f.write("\\title{" + name + "}\n")
-            with open('c/___' + folder, "w+") as f:
-                f.write("")
-            with open('c/_name', "w+") as f:
-                f.write(folder)
-            with open('c/_lectures.json', "w+") as f:
-                f.write("{}")
+            utilities.CreateCourseFolder(folder)
         else:
             print("Kurs już istnieje")
     else:
--- utilities.py
+++ utilities.py
@@ -27,17 +27,50 @@ def RewriteFileUncommented(filename):
     with open(filename, "w") as fil:
         fil.write(content)
 
+# odpowiada za dodanie nowego wykładu do plików
 def AppendNewLecture(lectures,lectureNumber):
-    with open('c/_lectures.json', "w+") as f:
+    with open('c/l' + str(len(lectures) ) + '.tex', "w+") as f: # tworzy nowy plik tex
+        f.write("")
+
+    with open('c/_lectures.json', "w+") as f: # dodaje plik to json
         f.write(json.dumps(lectures))
 
-    f = open("c\\master.tex", "r")
+    f = open("c\\master.tex", "r") # wczytuje zawartość
     contents = f.readlines()
     f.close()
 
-    contents.insert(len(contents) - 1, " \input{l" + str(lectureNumber) + ".tex}\n")
+    contents.insert(len(contents) - 1, " \input{l" + str(lectureNumber) + ".tex}\n") # dorzuca linijkę łączącą nowoutworzony plik
 
-    f = open(r"c\\master.tex", "w")
+    f = open(r"c\\master.tex", "w") # zapisuje nową zawartość pliku
     contents = "".join(contents)
     f.write(contents)
     f.close()
+
+# tworzy folder dla nowego kursu, folder - nazwa folderu do utworzenia
+def CreateCourseFolder(folder):
+    print("cmd /c mkdir lic-4/" + folder)
+    os.system("cmd /c mkdir lic-4\\" + folder)
+    os.system("cmd /c rmdir c")
+    os.system("cmd /c mklink /j c \"lic-4\\" + folder + "\"")
+    with open('c/master.tex', "w+") as f:
+        f.write(
+            "\\documentclass[12pt]{article}\n\\input{../preamble.tex}\n\\input{coursepreamble.tex}\n\\begin{document}\n\n\\end{document}")
+    with open('c/coursepreamble.tex', "w+") as f:
+        f.write("\\title{" + name + "}\n")
+    with open('c/___' + folder, "w+") as f:
+        f.write("")
+    with open('c/_name', "w+") as f:
+        f.write(folder)
+    with open('c/_lectures.json', "w+") as f:
+        f.write("{}")
+       
+# zwraca listę, zawierającą kolejno mapowania folder->nazwa i id->folder dla kursów
+def GetCoursesMapping():
+    with open('lic-4/courses.json') as f:
+        courses = json.loads(f.read())
+    iter = 0
+    courses_i = dict()
+    for i in courses:
+        iter = iter + 1
+        courses_i[str(iter)] = i
+    return [courses, courses_i]
